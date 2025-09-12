30c7b26a803f99947738b88091e749e2bee5d782
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-09-12 16:04:22 +1000

    Update 4TB.sh

--- 4TB.sh
+++ 4TB.sh
@@ -2,24 +2,26 @@
 lsblk -d -o NAME,SIZE,MODEL
 
 # Wipe the disk (replace sdb)
-wipefs -a /dev/sdb
-sgdisk --zap-all /dev/sdb
+wipefs -a /dev/nvme0n1
+sgdisk --zap-all /dev/nvme0n1
 
 # Create physical volume
-pvcreate /dev/sdb
+pvcreate /dev/nvme0n1
 
 # Create volume group using entire disk
-vgcreate vgdata /dev/sdb
+vgcreate vgdata /dev/nvme0n1
 
 # Create thin pool using all free space
 lvcreate --type thin-pool -l 100%FREE -n thinpool vgdata
 
 # Add to Proxmox
-pvesm add lvmthin lvm-data -vgname vgdata -thinpool thinpool
+pvesm add lvmthin nvme-storage --vgname vg_nvme_auto --thinpool thinpool_nvme_auto --content images,rootdir
 
 # Verify
 pvesm status
-
 lsblk /dev/nvme0n1
 mount | grep nvme0n1
 pvs | grep nvme0n1
+vgs
+lvs -a -o +seg_monitor
+

3287d5a9650c1de2f8b8b588cb093fcee6fb6b7d
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-09-16 20:05:10 +1000

    Update omakub.sh

--- omakub/omakub.sh
+++ omakub/omakub.sh
@@ -9,17 +9,17 @@ sudo apt install -y qemu-guest-agent
 sudo systemctl enable --now qemu-guest-agent
 
 # Allow RDP access
-gsettings set org.gnome.desktop.remote-desktop.rdp enable true
+#gsettings set org.gnome.desktop.remote-desktop.rdp enable true
 
 # Set RDP authentication (replace with your password)
-secret=$(echo -n "password" | base64)
-gsettings set org.gnome.desktop.remote-desktop.rdp password "$secret"
+#secret=$(echo -n "password" | base64)
+#gsettings set org.gnome.desktop.remote-desktop.rdp password "$secret"
 
 # Allow screen sharing promptless
-gsettings set org.gnome.desktop.remote-desktop.rdp view-only false
+#gsettings set org.gnome.desktop.remote-desktop.rdp view-only false
 
 # Enable required services
-systemctl --user enable --now gnome-remote-desktop.service
+#systemctl --user enable --now gnome-remote-desktop.service
 
 sudo systemctl stop ufw
 sudo systemctl disable ufw
@@ -27,5 +27,14 @@ sudo ufw disable
 
 sudo systemctl stop firewalld
 sudo systemctl disable firewalld
+
+sudo tee /etc/sysctl.d/10-disable-ipv6.conf > /dev/null <<EOF
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+EOF
+
+sudo sysctl --system
+
 
 wget -qO- https://omakub.org/install | bash

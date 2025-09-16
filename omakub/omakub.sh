537b250f4e229220f3fa7f98da73b394b75b0bb1
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-09-16 17:49:55 +1000

    Update omakub.sh

--- omakub/omakub.sh
+++ omakub/omakub.sh
@@ -2,6 +2,10 @@
 
 
 sudo apt update
+
+# VirtIO NIC and disk drivers are already included in modern Ubuntu kernels. You don’t need to install extra drivers manually.
+
 sudo apt install -y qemu-guest-agent
 sudo systemctl enable --now qemu-guest-agent
+
 wget -qO- https://omakub.org/install | bash

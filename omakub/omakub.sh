884c15cc83ff726b0bfeb90e8ed8f43d76b8782c
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-09-16 17:42:55 +1000

    Update and rename Omakub.sh to omakub.sh

--- omakub/omakub.sh
+++ omakub/omakub.sh
@@ -1,4 +1,4 @@
-# bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/vm/ubuntu2504-vm.sh)"
+# bash -c "$(curl -fsSL https://raw.githubusercontent.com/RockAfeller2013/proxmox_helperscripts/refs/heads/main/omakub/Omakub.sh)"
 
 
 sudo apt update

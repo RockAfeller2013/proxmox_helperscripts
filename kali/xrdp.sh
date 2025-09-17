011d29c14a85bee055f180b5aefde5cb4c9b2e73
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-09-17 23:53:54 +1000

    Create xrdp.sh

---
+++ kali/xrdp.sh
@@ -0,0 +1,16 @@
+apt-get update
+apt-get full-upgrade -y
+
+echo "net.ipv6.conf.all.disable_ipv6=1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6=1" >> /etc/sysctl.conf
+sysctl -p
+systemctl stop ufw || true
+systemctl disable ufw || true
+apt-get --yes install qemu-guest-agent kali-desktop-xfce xorg xrdp xorgxrdp
+bash -c "$(curl -fsSL https://gitlab.com/kalilinux/recipes/kali-scripts/-/raw/main/xfce4.sh)"
+sudo systemctl enable xrdp --now
+sudo systemctl enable xrdp-sesman --now
+sudo systemctl restart xrdp
+sudo systemctl restart xrdp-sesman
+echo 'kali:kali' | chpasswd
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/RockAfeller2013/proxmox_helperscripts/refs/heads/main/kali/rdpconfig.sh)"

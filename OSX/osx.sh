de9f2eb5fa1fa082852eaf46d669f8f4951b1414
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-10-02 15:21:45 +1000

    Update osx.sh

--- OSX/osx.sh
+++ OSX/osx.sh
@@ -1,7 +1,6 @@
 !/bin/bash
 # Create MacOS 15 Sequoia VM in Proxmox VE (Intel CPU only)
-# Usage: sudo bash create_macos_sequoia.sh
-# bash -c "$(curl -fsSL https://raw.githubusercontent.com/RockAfeller2013/proxmox_helperscripts/refs/heads/main/kali/kali_install.sh)"
+# sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/RockAfeller2013/proxmox_helperscripts/refs/heads/main/OSX/osx.sh)"
 
 VMID=1500
 VMNAME="MacOSSequoia"

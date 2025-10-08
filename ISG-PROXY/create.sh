1852d52db155ca8c1c88dcae9b0c0f1b53ce1d99
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-10-08 16:39:24 +1000

    Update create.sh

--- ISG-PROXY/create.sh
+++ ISG-PROXY/create.sh
@@ -1,18 +1,36 @@
-# Create the VM
-qm create 500 --name ISG-PROXY --memory 32000 --cores 2 --sockets 1 --numa 1 --net0 virtio,bridge=vmbr0 --bootdisk scsi0 --scsihw virtio-scsi-pci --agent 1
+# Create base VM
+qm create 500 --name ISG-PROXY --memory 32000 --cores 2 --sockets 1 --numa 1 --machine pc-i440fx-2.8
 
-# Import the QCOW2 image
+# Set boot options and devices
+qm set 500 --boot order=scsi0 --scsihw virtio-scsi-pci --agent 1 --balloon 0
+
+# Configure display and serial
+qm set 500 --serial0 socket --vga serial0
+
+# Set network (replace with your MAC or let Proxmox generate)
+qm set 500 --net0 virtio,bridge=vmbr0
+
+# Import and attach boot disk
 qm importdisk 500 /mnt/synology-backups/ISG-Proxy/ProxySG-SWG-KVM-Enterprise/ProxySG_SWG_KVM_303757.qcow2 local-lvm --format raw
-
-# Attach the boot disk
 qm set 500 --scsi0 local-lvm:vm-500-disk-0,format=raw
 
-# Set boot and display options
-qm set 500 --boot order=scsi0 --serial0 socket --vga vga0
+# Add data disks (ProxySG requirement)
+qm set 500 --scsi1 local-lvm:100G,format=raw
+qm set 500 --scsi2 local-lvm:100G,format=raw
 
-# Add two 100GB data disks (ProxySG requirement)
-qm set 500 --scsi1 local-lvm:100,format=raw
-qm set 500 --scsi2 local-lvm:100,format=raw
 
-# Optional: Configure network via DHCP
-qm set 500 --ipconfig0 ip=dhcp
+
+qm set 500 -agent 1
+qm set 500 -balloon 0
+qm set 500 -boot order=scsi0
+qm set 500 -cores 2
+qm set 500 -cpu host,flags=+xsave,+x2apic,-vmx
+qm set 500 -machine pc-i440fx
+qm set 500 -memory 32000
+qm set 500 -name ISG-PROXY
+qm set 500 -net0 virtio,bridge=vmbr0,queues=2
+qm set 500 -numa 1
+qm set 500 -scsihw virtio-scsi-pci
+qm set 500 -serial0 socket
+qm set 500 -sockets 1
+qm set 500 -vga serial0

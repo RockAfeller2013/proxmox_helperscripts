c023b1e27d7657b9de87580058698575d72ecb76
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-11-14 13:12:09 +1000

    Update ESXi.sh

--- ESXi/ESXi.sh
+++ ESXi/ESXi.sh
@@ -1,14 +1,18 @@
 qm create 4000 \
   --name esxi-test \
   --memory 32768 \
-  --cores 4 \
-  --cpu host \
+  --cores 2 \
+  --sockets 1 \
+  --cpu host,+vmx \
   --machine q35 \
   --bios ovmf \
-  --scsihw pvscsi \
-  --scsi0 local-lvm:100 \
-  --net0 vmxnet3,bridge=vmbr0 \
+  --scsihw sata-ahci \
+  --sata0 local-lvm:100 \
+  --net0 model=vmxnet3,bridge=vmbr0 \
   --ostype other \
-  --args "-cpu host"
+  --args "-cpu host,+vmx"
 
 qm set 4000 --cdrom /var/lib/vz/template/iso/VMware-VMvisor-Installer-8.0U3e-24677879.x86_64.iso
+qm set 4000 --boot order=sata0
+qm set 4000 --hugepages 2
+qm set 4000 --numa 1

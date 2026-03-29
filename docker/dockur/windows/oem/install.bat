e0087009068fe3fcadf7edbf96e822cbea74eb77
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2026-03-30 08:51:19 +1000

    Update install.bat

--- docker/dockur/windows/oem/install.bat
+++ docker/dockur/windows/oem/install.bat
@@ -1,6 +1,6 @@
 @echo off
 
-msiexec /q /i "C:\oem\data\sensor\installer_vista_win7_win8-64-4.1.0.5463.msi" /L* C:\oem\data\sensor\log.txt COMPANY_CODE=ZVFJP6Q4W5IJ0MN15L95
+msiexec /q /i "C:\oem\data\sensor\installer_vista_win7_win8-64-4.1.0.5463.msi" /L* C:\oem\data\sensor\log.txt COMPANY_CODE=
 
 # wusa "C:\oem\data\KB4474419\windows6.1-kb4474419-v3-x64_b5614c6cea5cb4e198717789633dca16308ef79c.msu" /quiet /norestart
 # wmic computersystem where name="windows7A" call rename name="NewHostname"

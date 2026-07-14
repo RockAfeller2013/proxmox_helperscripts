eec21bb5ba938864268ce2a92c4f8e9cd81172e0
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2026-07-14 21:41:35 +1000

    Update ubuntu-desktop.sh

--- omakub/ubuntu-desktop.sh
+++ omakub/ubuntu-desktop.sh
@@ -1,5 +1,9 @@
 # bash -c "$(curl -fsSL https://raw.githubusercontent.com/RockAfeller2013/proxmox_helperscripts/refs/heads/main/omakub/ubuntu-desktop.sh)"
 
+cd /var/lib/vz/template/iso/
+wget -O ubuntu-25.04-desktop-amd64.iso \
+  https://releases.ubuntu.com/25.04/ubuntu-25.04-desktop-amd64.iso
+
 qm create 5001 --name ubuntu-desktop --memory 4096 --cores 2 --sockets 1 --cpu host
 
 qm set 5001 --scsihw virtio-scsi-pci --scsi0 local-lvm:64

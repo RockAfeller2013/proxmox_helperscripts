918b2f318186eca3506f50706e3f893a97a2a4f1
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2026-07-29 10:23:22 +1000

    Update prepare-template.sh

--- omakub/UnbuntuDesktop/prepare-template.sh
+++ omakub/UnbuntuDesktop/prepare-template.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -e
 
+sudo apt install cloud-init qemu-guest-agent
+
 # Set template hostname
 hostnamectl set-hostname ubuntu-template
 
 # Remove old hostname references
 sed -i '/127.0.1.1/d' /etc/hosts
 echo "127.0.1.1 ubuntu-template" >> /etc/hosts
+
+# Reset machine identity
+cloud-init clean --machine-id || true
+
 
 # Clean machine-specific identifiers
 truncate -s 0 /etc/machine-id
@@ -28,8 +34,11 @@ apt clean
 
 # Remove shell history
 history -c
+unset HISTFILE
+
 rm -f /root/.bash_history
 rm -f ~/.bash_history
 
 echo "VM ready for templating. Shutdown now."
 shutdown -h now
+poweroff

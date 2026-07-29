4ca893d2f822024b98f83f9eca4ae4c21b8181a5
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2026-07-29 10:19:37 +1000

    Create prepare-template.sh

---
+++ omakub/UnbuntuDesktop/prepare-template.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+# Set template hostname
+hostnamectl set-hostname ubuntu-template
+
+# Remove old hostname references
+sed -i '/127.0.1.1/d' /etc/hosts
+echo "127.0.1.1 ubuntu-template" >> /etc/hosts
+
+# Clean machine-specific identifiers
+truncate -s 0 /etc/machine-id
+rm -f /var/lib/dbus/machine-id
+
+# Clean SSH host keys (regenerated on first boot)
+rm -f /etc/ssh/ssh_host_*
+
+# Clean logs
+journalctl --rotate
+journalctl --vacuum-time=1s
+
+# Clean temporary files
+rm -rf /tmp/*
+rm -rf /var/tmp/*
+
+# Clean apt cache
+apt clean
+
+# Remove shell history
+history -c
+rm -f /root/.bash_history
+rm -f ~/.bash_history
+
+echo "VM ready for templating. Shutdown now."
+shutdown -h now
